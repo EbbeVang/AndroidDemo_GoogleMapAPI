21e0b3c12e3851f7cf5374d98c6ec68180325b93
ebbevang <ev@evang.dk>
2013-04-15 21:27:45 +0200

    Added project to repository

---
+++ src/dk/lowtide/MapAPIv2/MapAPIv2.java
@@ -0,0 +1,14 @@
+package dk.lowtide.MapAPIv2;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+
+public class MapAPIv2 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_map_apiv2);
+	}
+}
